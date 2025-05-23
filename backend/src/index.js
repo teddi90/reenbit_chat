a0868c05e6209ab3b8e5289aeb9b740aed18968a
teddi90 <slobodyanoleg5@gmail.com>
2025-05-23 17:15:42 +0300

    completed build settings

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,15 @@ import chatRoutes from './routes/chat.route.js';
 import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
 
+import path from 'path';
+
 import {connectDB} from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({
@@ -19,6 +22,14 @@ app.use(cors({
 
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}`);
